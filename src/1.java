e7b8bf7a9a7d35626ae55114c09d436608c551ca
bouwscarmur557 <bouwscarmur557@gmail.com>
2025-03-06 13:54:24 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class RandomJavaCode{
+    public static void main(String[] args){
+        //This is a comment, it will be ignored by the compiler
+        int x = 5;
+        boolean y = true;
+        String z = "Hello World";
+        System.out.println("The value of x is: " + x);
+        System.out.println("The value of y is: " + y);
+        System.out.println("The value of z is: " + z);
+    }
+}
