7d684eaaeaa835d50045ac1bed27a1561c8c6588
bouwscarmur557 <bouwscarmur557@gmail.com>
2025-03-12 04:16:40 +0000

    Add 7.java

---
+++ src/7.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+public static void main(String[] args) {
+Random r = new Random();
+int num1 = r.nextInt(20);
+System.out.println("The random number is: " + num1);
+}
+}
