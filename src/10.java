5419a3dedce2548a847d0bf5d9e4e438c24509bd
bouwscarmur557 <bouwscarmur557@gmail.com>
2025-03-15 06:39:44 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class GithubSchoolProject {
+  public static void main(String[] args) {
+    Random random = new Random();
+    int x = random.nextInt(10);
+    System.out.println("The random number is: " + x);
+  }
+}
