e064ccd8e4b5dc1dfa9360b9ba32c24528e546c7
bouwscarmur557 <bouwscarmur557@gmail.com>
2025-03-19 04:40:45 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class RandomJavaCode {
+public static void main(String[] args) {
+int x = 5; // Random number between 1 and 10
+System.out.println("The random number is: " + (x));
+}
+}
