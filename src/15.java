fd99d9651cd6a56a7b9a971b9d2fde5a81120750
bouwscarmur557 <bouwscarmur557@gmail.com>
2025-03-20 17:53:53 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example code to demonstrate basic Java functionality
+        System.out.println("Hello, World!");
+    }
+}
