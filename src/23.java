1bcf9e8c3cd8eccabed2d98dc80bc99fea9bf3eb
bouwscarmur557 <bouwscarmur557@gmail.com>
2025-04-02 10:31:06 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,8 @@
+import com.github.kittininnov.rocketsdk.Rocket;
+
+public class RockETSample {
+
+    public static void main(String[] args) {
+        // Your rocket creation and flight management code here
+    }
+}
