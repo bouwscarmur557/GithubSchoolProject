ae4994aaaeb64452298e363c72985193b256dfb0
bouwscarmur557 <bouwscarmur557@gmail.com>
2025-04-08 05:22:38 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,6 @@
+public class MyJavaClass {
+    public static void main(String[] args) {
+        // This is just an example of how you can write Java code in your GitHub SchoolProject repository.
+        System.out.println("Hello, World!");
+    }
+}
