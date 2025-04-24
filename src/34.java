2e431928d83ba9bfbf776a07199f4fedb6bf2b04
bouwscarmur557 <bouwscarmur557@gmail.com>
2025-04-24 15:08:15 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,9 @@
+public class GithubSchoolProject {
+    public static void main(String[] args) {
+        // Code to run in the Github School Project repository
+        System.out.println("Hello, this is the first line of your code.");
+        System.out.println("You can add more lines here.");
+        System.out.println("Feel free to make any changes you want.");
+        System.out.println("Don't forget to push your changes back to the GitHub Repository!");
+    }
+}
